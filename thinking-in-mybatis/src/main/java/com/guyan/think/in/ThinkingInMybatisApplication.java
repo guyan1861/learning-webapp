15fa6458cd396adbe39f401a7adec2312820b293
guyan <wangjiang_nan@126.com>
2022-04-01 23:50:08 +0800

    springBoot整合 mybatis框架

---
+++ thinking-in-mybatis/src/main/java/com/guyan/think/in/ThinkingInMybatisApplication.java
@@ -0,0 +1,15 @@
+package com.guyan.think.in;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.guyan.think.in")
+public class ThinkingInMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkingInMybatisApplication.class, args);
+	}
+
+}
---
+++ thinking-in-mybatis/src/test/java/com/guyan/think/in/ThinkingInMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guyan.think.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkingInMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
